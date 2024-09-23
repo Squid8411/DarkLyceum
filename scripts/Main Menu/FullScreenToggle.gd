11bffdf6c54ca370570c669a45b2d88d6df6ff77
Squid8411 <othergenerics@gmail.com>
2024-09-23 10:59:53 +0200

    Made working placeholder scenes for Tutorial, Settings and Credits

---
+++ scripts/Main Menu/FullScreenToggle.gd
@@ -0,0 +1,7 @@
+extends CheckButton
+
+func _on_toggled(toggled_on: bool):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
