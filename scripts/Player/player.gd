6271ed12372440433f7c03003b6ded45ccd4439c
Squid8411 <othergenerics@gmail.com>
2024-10-07 10:58:06 +0200

    Added a Camera2D node that follows the player + terrain element

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -1,25 +1,11 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@export_range(0, 100, .2, "or_greater") var move_speed : float = 50.0
+@export var actions : PlayerInputActions
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("run_left", "run_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction = Input.get_vector(actions.left, actions.right, actions.up, actions.down)
+	
+	velocity.x = move_speed * direction.x
+	
 	move_and_slide()
---
+++ scripts/Player/player_input_actions.gd
@@ -0,0 +1,8 @@
+class_name PlayerInputActions
+extends Resource
+## Defines action names for the player input during gameplay.
+
+@export var left 	: StringName = "left"
+@export var right 	: StringName = "right"
+@export var up 		: StringName = "up"
+@export var down 	: StringName = "down"
