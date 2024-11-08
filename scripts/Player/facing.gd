9eccb4549def36f182a90631fec2dbef31f7a067
Squid8411 <othergenerics@gmail.com>
2024-11-08 14:00:11 +0100

    Fixed the player direction facing code

--- scripts/Player/facing.gd
+++ scripts/Player/facing.gd
@@ -1,7 +1,7 @@
 class_name Facing
 extends Node2D
 
-@export var character : PlatformerCharacter2D
+@export var character : PlatformerCharacter2D :
 	set(value):
 		if is_instance_valid(character):
 			character.direction_changed.disconnect(_on_direction_changed)
