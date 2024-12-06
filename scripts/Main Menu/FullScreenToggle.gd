3794460451592ffde20fd7d3025d4bbc015179d3
Squid8411 <othergenerics@gmail.com>
2024-12-06 14:33:54 +0100

    Removed fullscreen button focus and added a persistent state

--- scripts/Main Menu/FullScreenToggle.gd
+++ scripts/Main Menu/FullScreenToggle.gd
@@ -1,5 +1,9 @@
 extends CheckButton
 
+func _ready():
+	if DisplayServer.window_get_mode() == 3:
+		button_pressed = true
+
 func _on_toggled(toggled_on: bool):
 	if button_pressed == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
