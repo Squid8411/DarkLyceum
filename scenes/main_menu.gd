9ee6c79ad698472b969ecc65de7310e851fa16c1
Squid8411 <othergenerics@gmail.com>
2024-12-13 14:13:57 +0100

    Fixed fullscreen toggle syncing with current game window state

---
+++ scenes/main_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _process(delta):
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() == 3:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		elif DisplayServer.window_get_mode() == 0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
