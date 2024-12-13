2162ea295208726d0863240825b6bd75e04a9639
Squid8411 <othergenerics@gmail.com>
2024-12-13 14:54:16 +0100

    Added health bar, stamina bar and enemy (taurus) textures to sprites folder

--- scripts/Credits/credits.gd
+++ scripts/Credits/credits.gd
@@ -1,6 +1,8 @@
 extends Control
 
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("back"):
+		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
 	if Input.is_action_just_pressed("fullscreen"):
 		if DisplayServer.window_get_mode() == 3:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
