b2aa757f51c7c05af008156415e3ddc10f27f433
Squid8411 <othergenerics@gmail.com>
2025-01-06 10:48:09 +0100

    Added ESC to quit functionality on Main Menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -9,3 +9,6 @@ func _process(delta: float) -> void:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		elif DisplayServer.window_get_mode() == 0:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			
+	if Input.is_action_just_pressed("back"):
+		get_tree().quit()
